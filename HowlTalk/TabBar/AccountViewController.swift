8e6b79f731ebfa0de7e327a2d253d38ba199b977
koserim <btobang1@naver.com>
2020-07-13 14:56:12 +0900

    Account 화면 수정

--- HowlTalk/TabBar/AccountViewController.swift
+++ HowlTalk/TabBar/AccountViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 import Firebase
+import Kingfisher
 
 class AccountViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    @IBOutlet weak var statusMessageLabel: UILabel!
     @IBOutlet weak var statusMessageButton: UIButton!
+    
+    var uid: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.uid = Auth.auth().currentUser?.uid
         statusMessageButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+        setInfo()
+    }
+    
+    func setInfo() {
+        Database.database().reference().child("users").child(self.uid).observeSingleEvent(of: DataEventType.value) { (datasnapshot) in
+            let dic = datasnapshot.value as! [String:Any]
+            let userModel = UserModel(JSON: dic)
+            let url = URL(string: (userModel?.profileImageURL)!)
+            self.profileImageView.kf.setImage(with: url)
+            self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width/2
+            self.profileImageView.layer.masksToBounds = true
+            self.nameLabel.text = userModel?.userName
+            if (userModel?.status == nil || userModel?.status == "") {
+                self.statusMessageLabel.text = "상태메세지가 없습니다."
+                self.statusMessageLabel.textColor = UIColor.gray
+            } else {
+                self.statusMessageLabel.text = userModel?.status
+                self.statusMessageLabel.textColor = UIColor.black
+            }
+            self.mailLabel.text = Auth.auth().currentUser?.email
+        }
+        
     }
     
     @objc func showAlert() {
@@ -26,9 +59,9 @@ class AccountViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
             if let textfield = alertController.textFields?.first {
                 let dic = ["status":textfield.text!]
-                let uid = Auth.auth().currentUser?.uid
-                Database.database().reference().child("users").child(uid!).updateChildValues(dic)
+                Database.database().reference().child("users").child(self.uid).updateChildValues(dic)
             }
+            self.setInfo()
         }))
         alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { (action) in
             
